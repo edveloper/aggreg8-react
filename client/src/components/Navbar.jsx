c304d32948591c73a1c5ff333fec64bfcd2dc8ba
edveloper <edhardest@gmail.com>
2023-11-20 19:16:45 +0300

    Initial commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import Logo from "../img/logo.png";
+
+const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+          <img src={Logo} alt="" />
+          </Link>
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=insurance">
+            <h6>INSURANCE</h6>
+          </Link>
+          <Link className="link" to="/?cat=business">
+            <h6>BUSINESS</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?cat=law">
+            <h6>LAW</h6>
+          </Link>
+          <Link className="link" to="/?cat=fitness">
+            <h6>FITNESS</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
